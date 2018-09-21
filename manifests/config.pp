284e8b8a92b97677a2cd97df5eb03ab53ef1e417
root <root@aplusnunes85.mylabserver.com>
2018-09-21 01:20:59 +0000

    add the ssh config file

---
+++ manifests/config.pp
@@ -0,0 +1,9 @@
+class ssh::config {
+  file { '/etc/ssh/sshd_config': 
+    ensure => file,
+    mode   => '0600',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet///modules/ssh/files/sshd_config'
+ }
+}
